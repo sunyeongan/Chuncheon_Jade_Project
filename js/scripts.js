8ec4a53797364a1189ff279fc694e425fe5e76c7
seonyeong <asy100@naver.com>
2022-05-27 15:24:32 +0900

    ver.1 code upload

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+/*!
+* Start Bootstrap - Modern Business v5.0.6 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+$(document).ready(function(){
+    $('.count').prop('disabled', true);
+       $(document).on('click','.plus',function(){
+        $('.count').val(parseInt($('.count').val()) + 1 );
+    });
+    $(document).on('click','.minus',function(){
+        $('.count').val(parseInt($('.count').val()) - 1 );
+            if ($('.count').val() == 0) {
+                $('.count').val(1);
+            }
+        });
+ });
